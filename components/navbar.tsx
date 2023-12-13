5a7dde95a2591091eebd6fd24812812956bd321e
Hyesu <skxldzm123@naver.com>
2023-12-13 16:19:34 +0900

    deploy2

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -300,8 +300,8 @@ export default function Layout({
                       <path
                         d="M3 10H21M10 3V21M3 5C3 4.46957 3.21071 3.96086 3.58579 3.58579C3.96086 3.21071 4.46957 3 5 3H19C19.5304 3 20.0391 3.21071 20.4142 3.58579C20.7893 3.96086 21 4.46957 21 5V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V5Z"
                         stroke="white"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                       />
                     </g>
                   </g>
@@ -354,8 +354,8 @@ export default function Layout({
                       <path
                         d="M10.875 7.5H16.5M16.5 12H10.875M16.5 16.5H10.875M7.5 7.5H7.50113M7.5 12H7.50113M7.5 16.5H7.50113M5.25 21H18.75C19.3467 21 19.919 20.7629 20.341 20.341C20.7629 19.919 21 19.3467 21 18.75V5.25C21 4.65326 20.7629 4.08097 20.341 3.65901C19.919 3.23705 19.3467 3 18.75 3H5.25C4.65326 3 4.08097 3.23705 3.65901 3.65901C3.23705 4.08097 3 4.65326 3 5.25V18.75C3 19.3467 3.23705 19.919 3.65901 20.341C4.08097 20.7629 4.65326 21 5.25 21Z"
                         stroke="white"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                       />
                     </g>
                   </g>
@@ -405,8 +405,8 @@ export default function Layout({
                       d="M10.6154 13.3846L3 9.92308L21 3L14.0769 21L10.6154 13.3846ZM10.6154 13.3846L14.7692 9.23077"
                       stroke="#C62917"
                       stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     />
                   </g>
                 </svg>
